52491eafea18b0b55f5df10a5e128cb591607b53
sachin singh <sachinpatel9207811@gmail.com>
2026-05-06 22:18:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
-//const express = require('express');
-//const dotenv = require('dotenv');
-//const cors = require('cors');
-//const connectDB = require('./config/db');
-//const mobileRoutes = require('./routes/mobileRoutes');
+
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import  connectDB  from './config/db.js'
 import mobileRoutes from './routes/mobileRoutes.js'
 import authRoutes from './routes/authRoutes.js'
-//const authRoutes=require('./routes/authRoutes');
+
 dotenv.config();
 
 // Connect to Database
@@ -22,10 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-
-
 app.use('/api/auth', authRoutes)
-
 app.use('/api/mobiles', mobileRoutes);
 
 
